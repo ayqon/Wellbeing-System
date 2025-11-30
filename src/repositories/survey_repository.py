2a79be78b10038c2be714d72cbc04f8845d16aac
kwjlhh <kermit3419com@126.com>
2025-11-30 16:57:30 +0000

    Zheyu Day4 Dev4

--- src/repositories/survey_repository.py
+++ src/repositories/survey_repository.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from src.repositories.base import SqlAlchemyRepository
 from src.models.survey import WellbeingSurvey
 from sqlalchemy.orm import Session
@@ -11,3 +11,11 @@ class SurveyRepository(SqlAlchemyRepository[WellbeingSurvey]):
         return self.session.query(WellbeingSurvey).filter_by(
             student_id=student_id, week=week, year=year
         ).first()
+    
+    def get_by_student(self, student_id: str) -> List[WellbeingSurvey]:
+        """
+        Get all surveys for a specific student, ordered chronologically
+        """
+        return self.session.query(WellbeingSurvey).filter_by(
+            student_id=student_id
+        ).order_by(WellbeingSurvey.created_at).all()
