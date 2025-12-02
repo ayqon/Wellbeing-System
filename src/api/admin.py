c0d781866031f60040662b529de5b69006bcf65d
Muhammad Ibne Muzammil <mim262001@gmail.com>
2025-12-02 00:45:42 +0000

    Day 4 Nightly Fixes: Cleanup JS and add Admin TODOs

--- src/api/admin.py
+++ src/api/admin.py
@@ -4,8 +4,10 @@ admin_bp = Blueprint('admin', __name__)
 
 @admin_bp.route('/import', methods=['GET', 'POST'])
 def import_users():
+    # TODO: Implement actual import logic using ImportService
     return "Import Users Placeholder"
 
 @admin_bp.route('/import/academic', methods=['GET', 'POST'])
 def import_academic():
+    # TODO: Implement actual import logic using ImportService
     return "Import Academic Placeholder"
